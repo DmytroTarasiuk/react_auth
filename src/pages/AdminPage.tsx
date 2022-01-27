1699bed21bd00a8a73ba96de0c86fcfbc084d87a
DmytroTarasiuk <dmytro.tarasiuk1899@gmail.com>
2022-01-27 20:21:24 +0100

    adding chakra styles

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -4,15 +4,21 @@ import Menu from "../components/Menu/Menu";
 import Orders from "../components/Orders/Orders";
 import classes from '../components/Layout/Layout.module.css'
 
+import { Grid, GridItem } from '@chakra-ui/react'
+
 
 const AdminPage: React.FC = () => {
     return (
       <>
         <Header />
-        <main className={classes.main}>
-        <Menu />
-        <Orders />
-        </main>
+        <Grid h='90vh' templateRows='repeat(2, 1fr)' templateColumns='repeat(6, 1fr)' gap={6} className={classes.main}>
+          <GridItem rowSpan={2} colSpan={2} bg='bisque'>
+            <Menu />
+          </GridItem>
+          <GridItem rowSpan={2} colSpan={4} bg='tomato'>
+            <Orders />
+          </GridItem>
+        </Grid>
       </>
     )
 }
